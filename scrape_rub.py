cd45d3665ae6d8ae26ae09b6467c3db8b478c056
Yury Lobkov <racoon.enot@gmail.com>
2022-12-03 12:43:32 -0300

    add module to scrape rub rates

---
+++ scrape_rub.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+import time
+
+
+def get_rub_rate_str():
+    res = requests.get('https://www.sravni.ru/bank/sberbank-rossii/valjuty/')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    print('sravni.ru', res)
+    return soup.select('td:nth-child(2) > span:nth-child(1)')[0].getText()
+
+
+def get_rub_value():
+    return (float(get_rub_rate_str()[:5].replace(',','.')))
+
+
+#print(get_rub_rate_str())
+print(get_rub_value())
